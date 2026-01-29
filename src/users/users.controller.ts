fd947cfe39f02b5f8b488f20ac9b82def9ac3162
ghatoleakruti <ghatole.a@northeastern.edu>
2026-01-28 19:39:15 -0500

    first setup

---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+ 
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+ 
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+ 
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User | null> {
+    return this.usersService.findOne(+id);
+  }
+}
+ 
+ 
